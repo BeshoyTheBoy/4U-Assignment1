19baf9ea5646ca403b954c947754f22bca909974
awadb3223 <awadb3223@HRH-IC0027310.wrdsb.ca>
2017-09-13 14:07:13 -0400

    Finished finding when it will double. Still need to do when will balance reach a million

--- src/assignment_1/A1Q8.java
+++ src/assignment_1/A1Q8.java
@@ -4,16 +4,36 @@
  */
 package assignment_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author awadb3223
  */
 public class A1Q8 {
 
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Create a scanner
+        Scanner in = new Scanner(System.in);
+        //Ask user for values
+        System.out.println("Interest Rate in decimal form:");
+        double r = in.nextDouble();
+        System.out.println("Initial Balance:");
+        double balance = in.nextDouble();
+        //initial year
+        int year = 0;
+        //the double of initial amount
+        double mult = balance * 2;
+        
+        while (balance < mult) {
+            balance = (1+r)*balance;
+            year = year + 1;
+        }
+        System.out.println("The money will double in " + year + " years.");
     }
 }
